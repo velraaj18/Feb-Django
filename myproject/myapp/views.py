ac34e7d3590fa610ac51f3a62437f84638d42e85
VELRAAJ A S <velraaj30@gmail.com>
2024-02-26 09:45:52 +0530

    Now

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-   context ={
-      'name': 'vel',
-      'age' : 21,
-      'Degree': 'B.E'
-   } 
-   return render(request ,'index.html', context)
+   return render(request ,'index.html')
+
+def counter(request):
+   text = request.POST['text']
+   amount_of_words = len(text.split())
+   return render(request, 'counter.html', {'amount_of_words' : amount_of_words})
